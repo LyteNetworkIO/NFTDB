9fe867907246e7302c6feb7f9db36a3f828b0b64
Lyte <andygolay@gmail.com>
2022-07-18 14:52:16 +0000

    created package

---
+++ package/index.js
@@ -0,0 +1,8 @@
+function nftdb(data, operation) {
+
+    //iterate over the array of paymails, and pay out the respective amount.
+    
+    console.log(data+" has been processed via "+operation);
+}
+
+module.exports = nftdb
